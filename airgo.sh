243d0bf9876b5913c786d42ac2637dd099fd832c
sorc <sorc@qq.com>
2025-04-16 20:01:45 +0800

    修复错误的路径

--- airgo.sh
+++ airgo.sh
@@ -5,7 +5,7 @@ if [ ! -f www/dist/index.html ]; then
   # 创建dist目录
   mkdir -p www/dist
   # 创建index.html文件
-    echo "ok" > www/sdist/index.html
+    echo "ok" > www/dist/index.html
 fi
 # 判断www/dist/static是否存在
 if [ ! -d www/dist/static ]; then
